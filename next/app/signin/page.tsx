c1e021e16ed7887baeb4ef219ea5d5b06de24739
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-25 13:41:06 +0530

    Latest

--- next/app/signin/page.tsx
+++ next/app/signin/page.tsx
@@ -1,5 +1,16 @@
+"use client"
+
+import axios from "axios"
+
 export default function Signin(){
-    return <div>
-        signin page
+    return <div className='w-screen h-screen flex justify-center items-center'>
+        <div className='border p-2'>
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>
+            <button onClick={()=>{
+                axios.post("http://localhost:3000/api/v1/signin")
+            }}>Sign in</button>
+        </div>
+
     </div>
 }
---
+++ next/app/signup/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import axios from "axios"
+import { useState } from "react"
+import { useFormState } from "react-dom"
+
+export default function Signup(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    return <div className='w-screen h-screen flex justify-center items-center'>
+        <div className='border p-2'>
+            <input 
+            onChange={e => {setUsername(e.target.value)}}
+            type="text" 
+            placeholder="username"/>
+            <input 
+            onChange={e => {setPassword(e.target.value)}}
+            type="password" 
+            placeholder="password"/>
+            <button onClick={()=>{
+                axios.post("http://localhost:3000/api/v1/signup", {
+                    username,
+                    password
+                })
+            }}>Sign up</button>
+        </div>
+
+    </div>
+}
