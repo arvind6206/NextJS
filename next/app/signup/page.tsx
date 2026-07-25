3ffc9d734790b784f27a2edbb53342737032aa39
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-25 14:27:31 +0530

    latest

--- next/app/signup/page.tsx
+++ next/app/signup/page.tsx
@@ -1,30 +1,53 @@
-"use client"
+"use client";
 
-import axios from "axios"
-import { useState } from "react"
-import { useFormState } from "react-dom"
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default function Signup(){
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+export default function Signup() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    return <div className='w-screen h-screen flex justify-center items-center'>
-        <div className='border p-2'>
-            <input 
-            onChange={e => {setUsername(e.target.value)}}
-            type="text" 
-            placeholder="username"/>
-            <input 
-            onChange={e => {setPassword(e.target.value)}}
-            type="password" 
-            placeholder="password"/>
-            <button onClick={()=>{
-                axios.post("http://localhost:3000/api/v1/signup", {
-                    username,
-                    password
-                })
-            }}>Sign up</button>
+    const router = useRouter();
+
+    const handleSignup = async () => {
+        if (!username || !password) {
+            alert("Please fill all fields");
+            return;
+        }
+
+        try {
+            await axios.post("http://localhost:3000/api/v1/signup", {
+                username,
+                password,
+            });
+
+            router.push("/signin");
+        } catch (err) {
+            console.error(err);
+            alert("Signup failed");
+        }
+    };
+
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="border p-5 flex flex-col gap-3">
+                <input
+                    type="text"
+                    placeholder="Username"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button onClick={handleSignup}>
+                    Sign Up
+                </button>
+            </div>
         </div>
-
-    </div>
+    );
 }
