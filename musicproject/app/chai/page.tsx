00b2a85eec8f8e81e1b57b9e60295bc116b10edd
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-08-23 09:09:15 +0530

    Navbar Added

--- musicproject/app/chai/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ChaiPage(){
-    return (
-        <h1>Chai Page</h1>
-    )
-}
---
+++ musicproject/app/components/Navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "../components/ui/navbar-menu";
+import { cn } from "../lib/utils";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed inset-x-0 top-10 z-50 mx-auto max-w-2xl", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All courses</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advance composition</HoveredLink>
+            <HoveredLink href="/courses">Song writing</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+}
+
+export default Navbar;
